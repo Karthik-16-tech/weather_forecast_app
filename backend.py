32188653e95fb66d3e1583819c9bfde81881b2f5
karthik <bhuvankarthikpalla@gmail.com>
2025-02-17 00:05:47 +0530

    backend code

---
+++ backend.py
@@ -0,0 +1,19 @@
+import requests
+
+api_key = "330156023416a8c7d66bf0bea2c4651b"
+
+def get_data(place,forecast_days,kind):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo",forecast_days=3,kind="Temperature"))
